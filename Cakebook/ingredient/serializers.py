9eda606694fe804626b171dcaa21eb8c622260c5
Vetto Ola <vetto.ola@gmail.com>
2019-02-16 02:47:53 -0600

    Final basic Ingredient Model and urls DefaultRouter configuration done.

--- Cakebook/ingredient/serializers.py
+++ Cakebook/ingredient/serializers.py
@@ -1,22 +1,7 @@
 from rest_framework import serializers
 from ingredient.models import Ingredient
 
-class IngredientSerializer(serializers.Serializer):
-    id =  serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    description = serializers.CharField()
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return Ingredient.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.name = validated_data.get('name', instance.title)
-        instance.description = validated_data.get('description', instance.code)
-        instance.save()
-        return instance
+class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('url', 'name', 'description')
--- Cakebook/ingredient/views.py
+++ Cakebook/ingredient/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from ingredient.models import Ingredient
+from ingredient.serializers import IngredientSerializer
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
-# Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'ingredients': reverse('ingredient-list', request=request, format=format)
+    })
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
